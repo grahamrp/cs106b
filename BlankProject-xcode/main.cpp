38c0a026c81a68b19e26ae308df46b842ccfa962
Graham Parsons <graham@grahamrp.com>
2020-10-04 16:55:06 +0100

    Add blank project

---
+++ BlankProject-xcode/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main.cpp
+ * --------------
+ * This is an empty C++ source file.
+ */
+
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    return 0;
+}
